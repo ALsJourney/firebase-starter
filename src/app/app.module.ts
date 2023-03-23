8af8ef3a35cbb77dc3781c520187dcd3e76bbb0d
ALsJourney <aldin.dino@outlook.com>
2023-03-23 22:48:25 +0100

    Added firebase func

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./shared/shared.module";
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { environment } from '../environments/environment.development';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 @NgModule({
   declarations: [
@@ -18,7 +22,10 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCv6BhbHzvXvcdyFfQyArdrKwwEwb1K014",
+    authDomain: "firestarter-d3c06.firebaseapp.com",
+    projectId: "firestarter-d3c06",
+    storageBucket: "firestarter-d3c06.appspot.com",
+    messagingSenderId: "429180027859",
+    appId: "1:429180027859:web:10049b07fad8955fc8e526",
+    measurementId: "G-XQ421Z2KP0"
+  }
+};
